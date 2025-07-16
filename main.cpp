991e445c9798c8f29316b9443da7f1577ae3e1f2
izashchelkin <ihor.zashchelkin@osapiens.com>
2025-07-16 02:44:39 +0200

    cleanup

--- main.cpp
+++ main.cpp
@@ -87,7 +87,7 @@ bool try_startup(const char *displayname, xcb_connection_t *&connection,
   if (error) {
     log_xcb_error(connection, error);
     if (error->error_code == 10)
-      std::cerr << "is another wm is already runting?" << std::endl;
+      std::cerr << "is another wm already runting?" << std::endl;
 
     return false;
   }
